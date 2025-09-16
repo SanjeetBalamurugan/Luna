981fcf1237c33bd7fdee50968198a749beb4c34e
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-16 23:11:57 +0530

    Create src/luna/hello.py

---
+++ src/luna/hello.py
@@ -0,0 +1,17 @@
+"""
+hello.py
+
+This module provides a simple hello_world function
+for demonstration purposes.
+"""
+
+def hello_world():
+    """
+    Prints 'Hello, World!' to the console.
+
+    Example:
+        >>> from my_rpi_project.hello import hello_world
+        >>> hello_world()
+        Hello, World!
+    """
+    print("Hello, World!")
