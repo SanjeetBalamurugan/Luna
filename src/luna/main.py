d0e7692e6eb72279efd32310f2370e69a5cd4a96
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-16 23:12:24 +0530

    Create main.py

---
+++ src/luna/main.py
@@ -0,0 +1,14 @@
+"""
+main.py
+
+Entry point for the Raspberry Pi project.
+"""
+
+from luna.hello import hello_world
+
+def main():
+    """Main function that runs the project."""
+    hello_world()
+
+if __name__ == "__main__":
+    main()
