bb462e3a24f1e0f1513fdaffa4cd77f946a95df4
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-17 21:52:29 +0530

    Update gen_api.py

--- docs/gen_api.py
+++ docs/gen_api.py
@@ -4,7 +4,10 @@ import mkdocs_gen_files
 src_dir = Path("src/luna")
 docs_dir = Path("reference")
 
-with mkdocs_gen_files.open(docs_dir / "index.md", "w") as f:
+index_page = docs_dir / "index.md"
+module_links = []
+
+with mkdocs_gen_files.open(index_page, "w") as f:
     print("# API Reference", file=f)
     print("\nThis section contains the API documentation for the `luna` package.", file=f)
 
@@ -16,9 +19,18 @@ for path in src_dir.rglob("*.py"):
 
     doc_path = docs_dir / module_path.with_suffix(".md")
     full_module_name = ".".join(module_path.parts)
+    
+    link_text = f"**`{full_module_name}`**"
+    link_url = f"{doc_path.name}"
+    module_links.append(f"- [{link_text}]({link_url})")
 
     with mkdocs_gen_files.open(doc_path, "w") as f:
         print(f"# `{full_module_name}`", file=f)
         print(f"\n::: {full_module_name}", file=f)
 
     mkdocs_gen_files.set_edit_path(doc_path, path)
+
+with mkdocs_gen_files.open(index_page, "a") as f:
+    f.write("\n")
+    for link in module_links:
+        f.write(link + "\n")
