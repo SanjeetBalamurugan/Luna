91f40c88c0e79621990cf0b37436a2991caa6311
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-17 20:27:15 +0530

    Update led_controller.py

--- src/luna/led_controller.py
+++ src/luna/led_controller.py
@@ -1,20 +1,18 @@
-
 import RPi.GPIO as GPIO
 import time
 
 class LEDController:
     def __init__(self, pin=18):
-            self.pin = pin
-                    GPIO.setmode(GPIO.BCM)
-                            GPIO.setup(self.pin, GPIO.OUT)
+        self.pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
 
-                                def blink(self, times=5, delay=1):
-                                        for _ in range(times):
-                                                    GPIO.output(self.pin, GPIO.HIGH)
-                                                                time.sleep(delay)
-                                                                            GPIO.output(self.pin, GPIO.LOW)
-                                                                                        time.sleep(delay)
+    def blink(self, times=5, delay=1):
+        for _ in range(times):
+            GPIO.output(self.pin, GPIO.HIGH)
+            time.sleep(delay)
+            GPIO.output(self.pin, GPIO.LOW)
+            time.sleep(delay)
 
-                                                                                            def cleanup(self):
-                                                                                                    GPIO.cleanup()
-
+    def cleanup(self):
+        GPIO.cleanup()
