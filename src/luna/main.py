668d8532ae95ec498b587011bedcea377c65b27d
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-17 20:29:08 +0530

    Update main.py
    led

--- src/luna/main.py
+++ src/luna/main.py
@@ -5,9 +5,14 @@ Entry point for the Raspberry Pi project.
 """
 
 from luna.hello import hello_world
+from luna.led_control import LEDController
 
 def main():
     """Main function that runs the project."""
+
+    led = LEDController(pin=9)
+    led.blink()
+    led.cleanup()
     hello_world()
 
 if __name__ == "__main__":
